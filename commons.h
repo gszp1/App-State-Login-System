927cd29dc1c0001cc6948c40e56ac4af4a2fb09d
gszp1 <grzybekmateusz@poczta.fm>
2023-12-10 19:20:01 +0100

    makes program POSIX compatibile

--- commons.h
+++ commons.h
@@ -1,9 +1,14 @@
+#define _POSIX_SOURCE
+
 #ifndef COMMONS_H
 #define COMMONS_H
 
 // includes //
-#include<stdio.h>
-#include<pthread.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <pthread.h>
+#include <signal.h>
+#include <unistd.h>
 
 // data types //
 
